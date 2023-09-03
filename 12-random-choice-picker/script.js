6bd2c59faef9f9ef33015cb6892e0f8c8163e31c
pala <pala@VirtualBox-0b135640.localdomain>
2023-09-03 11:58:21 +0200

    random choice picker project :sparkles:

---
+++ 12-random-choice-picker/script.js
@@ -0,0 +1,57 @@
+const choices = document.getElementById('choices')
+const tagsEl = document.getElementById('tags')
+
+
+choices.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+
+    if (e.key === 'Enter') {
+        randomSelect()
+    }
+})
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    tagsEl.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
+    })
+}
+
+function randomSelect() {
+    const times = 30
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unhighlightTag(randomTag)
+        }, 100)
+    }, 100)
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+            highlightTag(randomTag)
+        }, 100)
+    }, times * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unhighlightTag(tag) {
+    tag.classList.remove('highlight')
+}
