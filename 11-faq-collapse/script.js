7083e962a37be9ec6a0a8b42d7a6f75f09a58e4e
pala <pala@VirtualBox-0b135640.localdomain>
2023-09-03 10:53:14 +0200

    faq collapse project mejorado :sparkles:

--- 11-faq-collapse/script.js
+++ 11-faq-collapse/script.js
@@ -1,8 +1,14 @@
-const faqs = document.querySelectorAll('.faq');
+const faqs = document.querySelectorAll('.faq')
+const buttons = document.querySelectorAll('.faq button')
 
 faqs.forEach(faq => {
     faq.addEventListener('click', () => {
-        faq.classList.toggle('active');
-        faq.querySelector('.faq-answer').classList.toggle('active');
+        faq.classList.toggle('active')
+        faq.querySelector('.faq-answer').classList.toggle('active')
+        
+        buttons.forEach(button => {
+            if(button.parentElement === faq)
+                button.classList.toggle('open')
+        })
     });
 })
